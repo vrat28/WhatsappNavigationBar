e0db2bb117c3477d49847b579a6cf56831db2e91
Varun Rathi <varun.rathi@jeevansathi.com>
2020-04-22 00:15:08 +0530

    1. Navigation fix for right buttons with actions 2. Details view controller UI

--- WhatsappNavigation/Scenes/Chat Scene/ChatViewController.swift
+++ WhatsappNavigation/Scenes/Chat Scene/ChatViewController.swift
@@ -20,19 +20,34 @@ class ChatViewController: UIViewController {
         let backButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action:#selector(backPressed))
         if let contactView = Bundle.main.loadNibNamed("CustomContactHeader", owner: self, options: nil)?.first as? CustomContactHeader {
             let user = User(userName:"Chat User", image: "profilemale", isOnline: true)
-             contactView.setUserDetails(user)
-             let contactViewButton = UIBarButtonItem(customView: contactView)
-             navigationItem.leftBarButtonItems = [backButton]
-             navigationItem.titleView = contactView
-             
-         }
+            contactView.setUserDetails(user)
+            contactView.delegate = self
+            let contactViewButton = UIBarButtonItem(customView: contactView)
+            navigationItem.leftBarButtonItems = [backButton,contactViewButton]
+        }
+        
+        let callIcon = UIBarButtonItem(image: UIImage(named: "voice"), style: .plain, target: self, action: #selector(callClicked))
+        let videoIcon = UIBarButtonItem(image: UIImage(named: "video"), style: .plain, target: self, action: #selector(videoCallClicked))
+        navigationItem.rightBarButtonItems = [callIcon,videoIcon]
+    }
+    
+    @objc func callClicked(){
+        
+    }
+    
+    @objc func videoCallClicked(){
+    
     }
     
     @objc func backPressed(){
       
       
     }
-    
-   
-    
+}
+
+extension ChatViewController: ContactHeaderDelegate {
+    func contactClicked() {
+        let detailVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier:"DetailVC")
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
 }
